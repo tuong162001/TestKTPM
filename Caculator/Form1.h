c085e856a7826804d34150bcda3bdaa330e18dc0
tuong162001 <thaituong001@gmail.com>
2022-02-26 14:15:02 +0700

    them giao dien

--- Caculator/Form1.h
+++ Caculator/Form1.h
@@ -34,6 +34,12 @@ namespace Caculator {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^  label1;
+	protected: 
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Button^  button1;
 
 	private:
 		/// <summary>
@@ -48,11 +54,69 @@ namespace Caculator {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(60, 89);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(37, 17);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"So 1";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(167, 89);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(255, 22);
+			this->textBox1->TabIndex = 1;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(60, 182);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(37, 17);
+			this->label2->TabIndex = 0;
+			this->label2->Text = L"So 1";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(167, 182);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(255, 22);
+			this->textBox2->TabIndex = 1;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(174, 297);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(174, 59);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(669, 474);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
 	};
